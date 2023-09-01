b4d4d570d316e836150c27fd7b667e5dce7d4a02
Jay Barbhaiya <jay.barbhaiya@sap.com>
2023-09-01 12:57:13 +0200

    set leader+space to whichkey.show vscode command

--- lua/lazyvim/plugins/extras/vscode.lua
+++ lua/lazyvim/plugins/extras/vscode.lua
@@ -29,7 +29,7 @@ end
 vim.api.nvim_create_autocmd("User", {
   pattern = "LazyVimKeymaps",
   callback = function()
-    vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
+    vim.keymap.set("n", "<leader><space>", "[[<cmd>call VSCodeNotify('whichkey.show')<cr>]]")
     vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
     vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
   end,
